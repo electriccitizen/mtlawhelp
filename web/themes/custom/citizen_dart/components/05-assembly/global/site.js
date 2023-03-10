0c610879c9064573f6bc74cc9e029204ea1976cb
Trent Marks <trent@electriccitizen.com>
2023-03-10 11:06:16 -0500

    MTLH-101: Added close icon button to mobile main menu

--- web/themes/custom/citizen_dart/components/05-assembly/global/site.js
+++ web/themes/custom/citizen_dart/components/05-assembly/global/site.js
@@ -57,11 +57,17 @@ Drupal.behaviors.mainMenuToggle = {
         $('#block-main-menu > ul').addClass('toggled');
         $('.main-menu-toggle').addClass('toggled');
         $('body').addClass('toggled');
+        $('<a href="#" class="menu-close"><svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m12.65 36.45-1.1-1.1L22.9 24 11.55 12.65l1.1-1.1L24 22.9l11.35-11.35 1.1 1.1L25.1 24l11.35 11.35-1.1 1.1L24 25.1Z"/></svg></a>')
+          .prependTo('#block-main-menu > ul')
+          .click(function(){
+            $('.toggled').removeClass('toggled');
+            $('.menu-close').remove();
+          });
         $('<button class="button menu-close">Quick Exit</button>')
           .appendTo('#block-main-menu > ul')
           .click(function(){
             $('.toggled').removeClass('toggled');
-            $('.button.menu-close').remove();
+            $('.menu-close').remove();
         });
       });
     });
